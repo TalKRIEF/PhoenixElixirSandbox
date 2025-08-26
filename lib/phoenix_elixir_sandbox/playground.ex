8e54673e75bef83df5216841f111eb4e636c55fb
Tal KRIEF <talkrief@gmail.com>
2025-08-26 16:56:36 +0200

    def unwrap

--- lib/phoenix_elixir_sandbox/playground.ex
+++ lib/phoenix_elixir_sandbox/playground.ex
@@ -2,7 +2,7 @@ defmodule PhoenixElixirSandbox.Playground do
   def head_and_tail([head | tail]) do
     {:ok, %{head: head, tail: tail}}
   end
-end
 
-result = PhoenixElixirSandbox.Playground.head_and_tail([1,2,3])
-IO.puts(result)
+  def unwrap({:ok, value}), do: value
+  def unwrap({:error, msg}), do: "Error, #{msg}"
+end
