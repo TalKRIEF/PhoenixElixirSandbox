2de81f23f15a331bba73c61029850b0b7d305139
Tal KRIEF <talkrief@gmail.com>
2025-09-05 16:37:34 +0200

    function rendering view changed

--- lib/phoenix_elixir_sandbox_web/controllers/page_controller.ex
+++ lib/phoenix_elixir_sandbox_web/controllers/page_controller.ex
@@ -8,5 +8,6 @@ defmodule PhoenixElixirSandboxWeb.PageController do
   # This one actuaylly is an index but for testing purposes I renamed it
   def testplay(conn, _params) do
     render(conn, :testplay)
+    # 
   end
 end
