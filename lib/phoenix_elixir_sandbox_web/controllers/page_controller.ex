4b0b3d562441944502d7d506e743d07151afdd89
Tal KRIEF <talkrief@gmail.com>
2025-09-08 20:22:01 +0200

    crud generators

--- lib/phoenix_elixir_sandbox_web/controllers/page_controller.ex
+++ lib/phoenix_elixir_sandbox_web/controllers/page_controller.ex
@@ -8,6 +8,6 @@ defmodule PhoenixElixirSandboxWeb.PageController do
   # This one actuaylly is an index but for testing purposes I renamed it
   def testplay(conn, _params) do
     render(conn, :testplay)
-    # 
+    # Ecto find id
   end
 end
