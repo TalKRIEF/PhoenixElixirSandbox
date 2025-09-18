a55dc3f6f558676e9f4e2f52940f0b980c99e678
Tal KRIEF <talkrief@gmail.com>
2025-09-18 18:45:03 +0200

    seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,20 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias PhoenixElixirSandbox.Accounts
+alias PhoenixElixirSandbox.Repo
+
+
+Repo.delete_all(PhoenixElixirSandbox.User)
+IO.puts("________________________________________________All Users have been deleted___________________________________________")
+IO.puts("________________________________________________All Users have been deleted___________________________________________")
+IO.puts("________________________________________________All Users have been deleted___________________________________________")
+IO.puts("________________________________________________All Users have been deleted___________________________________________")
+
+Accounts.create_user(%{name: "Chloé", email: "chloe@yahoo.fr", password: "talouleplusbeau"})
+IO.puts("................................Chloé user created................................")
+Accounts.create_user(%{name: "Tal", email: "tal@gmail.com", password: "OOOOOO"})
+IO.puts("................................Tal user created................................")
+Accounts.create_user(%{name: "Teva", email: "tevakr@hotmail.fr", password: "BestTKintown"})
+IO.puts("................................Teva user created................................")
